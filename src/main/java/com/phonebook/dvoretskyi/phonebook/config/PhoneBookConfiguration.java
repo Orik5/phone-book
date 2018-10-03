b36c5124a74182ff1b900a66c4f99b0c37447437
OrestDvoretskyi <Or4yk5@gmail.com>
2018-10-03 13:34:00 +0300

    Description

--- src/main/java/com/phonebook/dvoretskyi/phonebook/config/PhoneBookConfiguration.java
+++ src/main/java/com/phonebook/dvoretskyi/phonebook/config/PhoneBookConfiguration.java
@@ -1,8 +1,13 @@
 package com.phonebook.dvoretskyi.phonebook.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class PhoneBookConfiguration {
+public class PhoneBookConfiguration implements WebMvcConfigurer {
+  public void addViewControllers(ViewControllerRegistry registry) {
 
+    registry.addViewController("/login").setViewName("login");
+  }
 }
