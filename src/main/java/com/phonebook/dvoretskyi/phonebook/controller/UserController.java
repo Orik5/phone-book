41b14c74c7a2a7209ee4c0560a94372ff2ef5aed
OrestDvoretskyi <Or4yk5@gmail.com>
2018-10-03 13:17:26 +0300

    add controller

--- src/main/java/com/phonebook/dvoretskyi/phonebook/controller/UserController.java
+++ src/main/java/com/phonebook/dvoretskyi/phonebook/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.phonebook.dvoretskyi.phonebook.controller;
 
 import com.phonebook.dvoretskyi.phonebook.services.UserService;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +11,9 @@ public class UserController {
 
   @Autowired
   UserService userService;
+  @GetMapping("/")
+  public String home(Map<String,Object> model){
+    return "home";
+  }
+
 }
